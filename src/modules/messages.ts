a97f600b069224abd6a23f8578c88bfc7e423c93
Kim Yewon <kyw017763@gmail.com>
2021-02-15 01:33:54 +0900

    Publish VSCode Extension Mover v1.0.3

--- src/modules/messages.ts
+++ src/modules/messages.ts
@@ -1,6 +1,6 @@
 export default {
     EXPORTATION_EXECUTION_ABORTED: 'Exportation execution aborted!',
-    EXPORTATION_EXECUTION_RESULT: (extensionCnt: number, exportResultStr: string) => `${extensionCnt} extension install commands are ${exportResultStr}!`,
+    EXPORTATION_EXECUTION_RESULT: (extensionCnt: number, exportResultStr: string) => `${extensionCnt} extensions' installation command are ${exportResultStr}!`,
     DELETING_HELP_EXECUTION_ABORTED: 'Deleting help execution aborted!',
     INVALID_VALUE: 'Invalid value entered!',
     IS_NOT_EXISTS: 'Directory does not exist!'
